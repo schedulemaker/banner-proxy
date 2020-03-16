a5bd6120d93fa87d6df6a2b84e05cc2813d85619
Paul Hutchings <paul@xps>
2020-03-16 18:27:36 -0400

    Added primitive lambda

---
+++ index/index.js
@@ -0,0 +1,40 @@
+'use-strict'
+
+if (!cache){
+    var cache = require('./cache');
+}
+
+exports.handler = async (event, context) => {
+    return checkCache(event) || await requestData(event);
+}
+
+function checkCache(event){
+    return null; //TODO - check cache
+}
+
+function createResources(){
+
+}
+
+async function requestData(event){
+    //Check that a banner object has been created for the school and there is a cache entry for the school
+    if (!cache.bannerCache[event.school]){
+        cache.bannerObjs[school] = new Banner(event.school);
+        cache.bannerCache[event.school] = {};
+    }
+
+    //Check there is a term entry for the school
+    if (!cache.bannerCache[event.school][event.term]){
+        cache.bannerCache[event.school][event.term] = {};
+    }
+
+    //perform the request to Banner for the data
+    let data = await cache.bannerObjs[school][event.method](event.params);
+    //put the data into the cache
+    cache.bannerCache[event.school][event.term][event.method] = {
+        timestamp: Date.now() / 1000,
+        data: data
+    }
+    //return the data
+    return data;
+}
