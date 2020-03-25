008ac95b7dc071eeec22da4922fd89394056092b
Paul Hutchings <paul@xps>
2020-03-25 19:48:57 -0400

    Changes to argument pass-through

--- index/index.js
+++ index/index.js
@@ -4,11 +4,11 @@ if (!cache){
     var cache = require('./cache');
 }
 
-exports.handler = async (event, context) => {
-    return checkCache(event) || await requestData(event);
+exports.handler = async ({school, term, method, params}, context) => {
+    return checkCache(school, term) || await requestData(school, term, method, params);
 }
 
-function checkCache(event){
+function checkCache(school, term){
     return null; //TODO - check cache
 }
 
@@ -16,22 +16,22 @@ function createResources(){
 
 }
 
-async function requestData(event){
+async function requestData(school, term, method, params){
     //Check that a banner object has been created for the school and there is a cache entry for the school
-    if (!cache.bannerCache[event.school]){
-        cache.bannerObjs[event.school] = new cache.Banner(event.school);
-        cache.bannerCache[event.school] = {};
+    if (!cache.bannerCache[school]){
+        cache.bannerObjs[school] = new cache.Banner(school);
+        cache.bannerCache[school] = {};
     }
 
     //Check there is a term entry for the school
-    if (!cache.bannerCache[event.school][event.term]){
-        cache.bannerCache[event.school][event.term] = {};
+    if (!cache.bannerCache[school][term]){
+        cache.bannerCache[school][term] = {};
     }
 
     //perform the request to Banner for the data
-    let data = await cache.bannerObjs[event.school][event.method](event.params);
+    let data = await cache.bannerObjs[school][method](params);
     //put the data into the cache
-    cache.bannerCache[event.school][event.term][event.method] = {
+    cache.bannerCache[school][term][method] = {
         timestamp: Date.now() / 1000,
         data: data
     }
