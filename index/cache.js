02f3b71b13119aa76ec23e9edae080aa3a8d05c4
Paul Hutchings <paul@xps>
2020-03-28 18:02:34 -0400

    Added /tmp directory caching support

--- index/cache.js
+++ index/cache.js
@@ -4,8 +4,16 @@ if (typeof(Banner) === 'undefined'){
     var Banner = require('banner');
 }
 
+if (typeof(fs) === 'undefined'){
+    var fs = require('fs');
+}
+
 module.exports = {
     Banner: Banner,
     bannerObjs: {},
-    bannerCache: {}
+    bannerCache: {},
+    fs: fs,
+    dir: '/tmp',
+    useTmp: false,
+    counter: 1
 }
--- index/index.js
+++ index/index.js
@@ -5,10 +5,18 @@ if (typeof(cache) === 'undefined'){
 }
 
 exports.handler = async ({school, term, method, params}, context) => {
-    return checkCache(school, term) || await requestData(school, term, method, params);
+    let data = checkCache(school, term, method) || checkTempDir(school, term, method);
+    if (data) return data;
+    else {
+        let request = requestData(school, term, method, params);
+        if (cache.counter % process.env.CHECK_FREQUENCY === 0){
+            checkRAM(context);
+        }
+        return await request;
+    }
 }
 
-function checkCache(school, term){
+function checkCache(school, term, method){
     let obj = cache.bannerCache[school][term][method];
 
     //Check if the object is in the cache
@@ -20,7 +28,7 @@ function checkCache(school, term){
     if ((Date.now() / 1000) - obj.timestamp < process.env.EXPIRE){
         return false
     }
-    else return obj;
+    else return obj.data;
 }
 
 function createCacheEntries(school, term){
@@ -36,19 +44,42 @@ function createCacheEntries(school, term){
     }
 }
 
+async function checkTempDir(school, term, method){
+    try {
+        let obj = await cache.fs.readFile(`${cache.dir}/${school}/${term}/${method}.json`, 'utf8');
+        return obj.data;
+    } catch (error) {
+        return false;
+    }
+}
+
+async function createTempDirEntries(school, term){
+    await cache.fs.mkdir(`${cache.dir}/${school}/${term}`, {recursive: true});
+}
+
 async function requestData(school, term, method, params){
     //perform the request to Banner for the data
     let request = cache.bannerObjs[school][method](params);
 
-    createCacheEntries(school, term);
+    cache.useTmp ? createCacheEntries(school, term) : createTempDirEntries(school, term);
 
     //put the data into the cache
     let data = await request;
-    cache.bannerCache[school][term][method] = {
+    let obj = {
         //Timestamp is stored as SECONDS
         timestamp: Date.now() / 1000,
         data: data
     }
+    cache.useTmp ? await cache.fs.writeFile(`${cache.dir}/${school}/${term}/${method}.json`, JSON.stringify(obj), 'utf8') 
+        : cache.bannerCache[school][term][method] = obj;
     //return the data
     return data;
+}
+
+/**
+ * Determines whether future cached entries should be written to the /tmp directory instead of stored in RAM
+ * @param {AWS.Lambda.Context} context 
+ */
+function checkRAM(context){
+    cache.useTmp = process.memoryUsage().rss / (context.memoryLimitInMB * 1024 * 1024) >= process.env.RAM_THRESHOLD;
 }
