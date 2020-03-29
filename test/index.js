96a461a57691cb82ba4ec61e1d7a71341bea328c
Paul Hutchings <paul@xps>
2020-03-29 11:35:52 -0400

    Added unit test suite outline

---
+++ test/index.js
@@ -0,0 +1,34 @@
+'use-strict'
+
+const assert = require('assert');
+const index = require('./index');
+
+process.env.RAM_THRESHOLD = 0.9;
+process.env.CHECK_FREQUENCY = 10;
+
+describe('Banner Proxy', function(){
+    describe('Handler', function(){
+        
+
+    });  
+
+    describe('checkCache', function(){
+
+    });
+
+    describe('checkTmpDir', function(){
+
+    });
+
+    describe('createCacheEntries', function(){
+
+    });
+
+    describe('createTmpDirEntries', function(){
+
+    });
+
+    describe('requestData', function(){
+
+    });
+});
